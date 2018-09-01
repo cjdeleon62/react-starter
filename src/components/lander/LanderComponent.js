e6ffdcd67f30c90aad0f380835a5cf4baa9bdcd4
Christopher De Leon <cdeleon@forbes.com>
2018-09-01 13:01:14 -0400

    lander page complete

--- src/components/lander/LanderComponent.js
+++ src/components/lander/LanderComponent.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LanderComponent = () => (
   <section className="lander">
-    <div className="lander__profile--container">
-      <div className="lander__profile--image"></div>
+    <h1 className="lander__jumbo text__xl">
+      Hi, my name is Chris.
+    </h1>
+    <div className="lander__text text__md">
+      <p>Jr. Front End Developer at Forbes Media .LLC</p>
+      <p>Creator of efficient, maintainable, and scalable code.</p>
+      <p>Still learning.</p>
     </div>
-    <div className="lander__text text__lg">
-      Spicy jalapeno biltong turducken cupim spare ribs short
-      ribs jerky, pork belly chicken pastrami shankle drumstick venison.
-      Shank venison strip steak shoulder kevin tenderloin ham short
-      ribs ham hock swine fatback chuck pork jerky prosciutto. Ball tip
-      bresaola chicken pancetta ham hock sirloin, jowl burgdoggen
-      cupim kielbasa. Picanha doner bresaola buffalo, bacon shoulder
-      strip steak tri-tip ball tip hamburger ham hock pancetta ribeye
-      tenderloin short loin. Buffalo pork belly fatback, kielbasa hamburger
-      bacon alcatra meatloaf pig drumstick jerky venison. Shoulder ball tip
-      salami chuck rump short loin burgdoggen hamburger cupim strip steak tail
-      buffalo capicola pig.
+    <div className="lander__links">
+      <Link to="/my-works">
+        <button className="lander__button lander__button--my-works text__sm btn">My Works</button>
+      </Link>
+      <Link to="/about">
+        <button className="lander__button lander__button--about text__sm btn">About Me</button>
+      </Link>
     </div>
   </section>
 );
